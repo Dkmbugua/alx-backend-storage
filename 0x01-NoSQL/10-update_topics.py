3171333213d345edaaa928605acf74e38c77f6f5
dkmbugua <dkmbugua70@gmail.com>
2024-12-15 18:06:25 +0300

    try

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+
+"""
+A python function that changes all topics of
+school document based on the name
+"""
+
+
+import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Changes all topics of a school document based on the name """
-    query = {"name": name}
-    new_values = {"$set": {"topics": topics}}
-
-    mongo_collection.update_many(query, new_values)
+    """
+    Change all topics of a school document
+    """
+    return mongo_collection.update_many({"name": name},
+                                        {"$set": {"topics": topics}})
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" 8-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
+"""
+A python fucntion that lists all documents
+in a collection
+"""
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """Return list of all docs in collection"""
+    if mongo_collection is None:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
